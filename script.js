648fb85d942cd43e5cf9545967faa6e62956af05
Rizky Fauzi Ilmi <rizkyfauziilmi@gmail.com>
2022-04-20 08:44:49 +0700

    section
    - section ~ profile

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 $(document).ready(function () {
+    // default
     $("#website div").hide();
-    $("#website div").css("opacity", "1");
+    $("#codepen div").hide();
+    $(".sub-skill").hide();
+    // topScroll function
+    $("#top").click(function () { 
+        $(window).scrollTop(0);
+    });
+    // height adjusment
+    let bannerHeight = $(".banner").height();
+    let projectsHeight = $("#projects").height();
+    let skillsHeight = $("#skills").height();
+    let profileHeight = $("#profile").height();
+    $("#projects").css("padding-top", bannerHeight);
+    $("#projects").css("padding-bottom", bannerHeight);
+    $("#skills").css("padding-top", projectsHeight);
+    $("#skills").css("padding-bottom", projectsHeight);
+    $("#profile").css("padding-top", skillsHeight);
+    $("#profile").css("padding-bottom", skillsHeight);
+    // display function
     $("#website a").click(function() { 
         $("#website div").slideToggle();
     });
-});
-$(document).ready(function () {
-    $("#codepen div").hide();
     $("#codepen a").click(function () { 
         $("#codepen div").slideToggle();
+    });
+    $(".skill").click(function () { 
+        $(".sub-skill").slideToggle();
     });
 });
