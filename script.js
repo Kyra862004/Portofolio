6554544a207800855444a299dc054ce9cbec0d0a
Rizky Fauzi Ilmi <104153142+RizkyFauziIlmi@users.noreply.github.com>
2022-04-22 14:34:17 +0700

    1.2 portofolio
    - skills page
    - margin & padding adjustment
    - toggle skills content

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+$(document).ready(function () {
+    // default toggle
+    $(".front-end").hide();
+    $(".back-end").hide();
+    // front end toggle
+    $("#front-end-toggle").click(function () { 
+        $(".front-end").slideToggle(500);
+    });
+    // back end toggle
+    $("#back-end-toggle").click(function () {
+        $(".back-end").slideToggle(500); 
+    });
+});
