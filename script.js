0b878e1dc6f1f5c0b017286152b60aef8c81cafd
Rizky Fauzi Ilmi <104153142+RizkyFauziIlmi@users.noreply.github.com>
2022-04-30 09:47:10 +0700

    hosting-new

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-$(document).ready(function () {
-    // default toggle
-    $(".front-end").hide();
-    $(".back-end").hide();
-    // front end toggle
-    $("#front-end-toggle").click(function () { 
-        $(".front-end").slideToggle(500);
-    });
-    // back end toggle
-    $("#back-end-toggle").click(function () {
-        $(".back-end").slideToggle(500); 
-    });
+$(document).ready(function(){
+	$('#nav-icon').click(function(){
+		$(this).toggleClass('open');
+        $('nav').toggleClass('slide');
+	});
 });
